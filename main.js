8479114ec27035042237487e0dd5e15399bb19e4
Jerome Chavez <jayrome12@yahoo.com>
2018-05-30 11:05:35 -0700

    Added a conditional to function that pauses the stop-watch if it running

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var $button = document.querySelector('.start-button')
 var $elapsedTime = document.querySelector('#elapsed-time')
 var isTimeRunning = false;
+var intervalTime
 
 function updateTime () {
   $elapsedTime.textContent = parseInt($elapsedTime.textContent) + 1
 }
 
-
 $button.addEventListener('click', function () {
-  isTimeRunning = !isTimeRunning
-  setInterval(updateTime, 1000)
+  if (isTimeRunning){
+    clearInterval(intervalTime)
+    isTimeRunning = false
+  }
+  else {
+    intervalTime = setInterval(updateTime, 1000)
+    isTimeRunning = true;
+  }
 })
